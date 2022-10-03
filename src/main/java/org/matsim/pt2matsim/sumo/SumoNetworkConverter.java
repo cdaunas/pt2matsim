ce112211a69deb754f51f4258a34bd2a0c4d3269
Geoffrey NEMOUCHY LAMRY <geoffrey.nemouchylamry@alten.com>
2022-10-03 15:05:57 +0200

    Revert "TEST 3 : add highway props"
    This reverts commit 3af05d75fbe66aaa95ebf7e7879d672a2cc2c25a.

--- src/main/java/org/matsim/pt2matsim/sumo/SumoNetworkConverter.java
+++ src/main/java/org/matsim/pt2matsim/sumo/SumoNetworkConverter.java
@@ -321,11 +321,9 @@ public class SumoNetworkConverter implements Callable<Integer> {
             }
 
             // set link prop based on MATSim defaults
-            LinkProperties prop = null;
             
-            if (edge.type.startsWith("highway"))
-            	prop = linkProperties.get(type.highway);
-            
+            LinkProperties prop = linkProperties.get(type.highway);
+             
             double speed = type.speed;
 
             // incoming lane connected to the others
